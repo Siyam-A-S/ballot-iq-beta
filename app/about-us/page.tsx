c6a1bb68f76efa879a090258f6c23f5671141f99
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2024-04-10 21:52:44 -0400

    Added a lot of ui components for almost all the pages

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,8 +1,40 @@
-
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+  
 export default async function AboutUs(){
     return(
     <main>
-    <h1 className="text-9xl">THIS IS ABOUT US</h1>        
+        <h1 className="text-9xl">
+            THIS IS ABOUT US
+        </h1> 
+        <div className="text-3xl flex min-h-screen flex-col items-center justify-between p-24">
+            <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+            </AccordionItem>
+            </Accordion>
+            
+
+        </div>   
+        <div className="text-3xl flex flex-col items-center justify-between p-24">
+            <Accordion type="single" collapsible>
+            <AccordionItem value="item-1">
+                <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                <AccordionContent>
+                Yes. It adheres to the WAI-ARIA design pattern.
+                </AccordionContent>
+            </AccordionItem>
+            </Accordion>
+            
+
+        </div>    
     </main>
     )
 }
