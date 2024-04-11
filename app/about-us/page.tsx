fad14d889d45eacf8c25da1524816fa02e2575f8
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2024-04-10 22:40:24 -0400

    Updates about us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,4 @@
-import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
+
   
 export default async function AboutUs(){
     return(
