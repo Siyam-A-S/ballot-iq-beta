f59bbba894686367f366a52d6545cb5fd9ab87df
Siyam-A-S <98270965+Siyam-A-S@users.noreply.github.com>
2024-04-10 22:26:33 -0400

    Made some changes to the github link

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -8,33 +8,25 @@ import {
 export default async function AboutUs(){
     return(
     <main>
-        <h1 className="text-9xl">
+        <h1 className="text-5xl">
             THIS IS ABOUT US
         </h1> 
         <div className="text-3xl flex min-h-screen flex-col items-center justify-between p-24">
-            <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-            </AccordionItem>
-            </Accordion>
-            
+            <p>
+            <h1>#Problem</h1>
+            We are tackling the problem of voter illiteracy and low voter-turn-out to ensure that each citizen can make informed decisions and feel involved in the whole election process. We are making a website which will display all the relevant information in an intuitive and beautiful way. Our work is aligned with the SDG16 which is Peace, Justice and Strong Institutions.
 
+            <h1>#APIs Used</h1>
+            <h1>1. Morphcast Emotion AI - A facial emotion recognition tool to track customer satisfaction in using the application and guide them accordingly.
+            Github link : https://github.com/MorphCast/mph-sdk-integration-react</h1>
+
+            2. OpenFEC API: openFEC API by FEC (Federal Election Commission) to gather all information regarding candidates
+            Link: https://api.open.fec.gov/developers/
+
+
+            </p>
         </div>   
-        <div className="text-3xl flex flex-col items-center justify-between p-24">
-            <Accordion type="single" collapsible>
-            <AccordionItem value="item-1">
-                <AccordionTrigger>Is it accessible?</AccordionTrigger>
-                <AccordionContent>
-                Yes. It adheres to the WAI-ARIA design pattern.
-                </AccordionContent>
-            </AccordionItem>
-            </Accordion>
-            
-
-        </div>    
+           
     </main>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,29 @@
 import { Link } from "lucide-react";
 import Image from "next/image";
 
-export function TypographyH3() {
-  return (
-    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-      The Joke Tax
-    </h3>
-  )
-}
 
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="className=mb-3 text-4xl font-semibold fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          The information you need 
+        <a
+        href="https://github.com/Siyam-A-S/ballot-iq-beta"
+        className="group  px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          
+        <p className="className=mb-3 text-2xl font-semibold fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Check out our github repository
         </p>
+        </a>
         <div className="mb-3 text-4xl font-semibold fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
             By Justice League
         </div>
       </div>
 
       <div className=" relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#e1b452] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">   
-        
           <h2 className= "text-9xl font-semibold">
             BallotIQ
           </h2>
